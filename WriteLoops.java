1e1722e69a5d4d980841829cecb8c1380e656414
asconem <asconem@gmail.com>
2020-02-09 12:56:02 -0500

    Completed loops.

--- WriteLoops.java
+++ WriteLoops.java
@@ -1,199 +1,200 @@
-                //import com.sun.org.apache.xpath.internal.SourceTree;
-    
-                import java.awt.SystemTray;
-                import java.util.concurrent.ThreadLocalRandom;
-                import java.util.function.Supplier;
-    
-                /**
-                 * Writeloops get you thinking about how to do different things with loops.
-                 *
-                 * @author anonymous coward
-                 * @version -0.3
-                 *
-                 */
-                public class WriteLoops {
-    
-                    private static final int _3 = 3;
-    
-                    public int oneToFive() {
-                        int w = 0;
-    
-                        // Write a FOR loop that counts from 1 to 10.
+                    //import com.sun.org.apache.xpath.internal.SourceTree;
+        
+                    import java.awt.SystemTray;
+                    import java.util.concurrent.ThreadLocalRandom;
+                    import java.util.function.Supplier;
+        
+                    /**
+                     * Writeloops get you thinking about how to do different things with loops.
+                     *
+                     * @author anonymous coward
+                     * @version -0.3
+                     *
+                     */
+                    public class WriteLoops {
+        
+                        private static final int _3 = 3;
+        
+                        public int oneToFive() {
+                            int w = 0;
+        
+                            // Write a FOR loop that counts from 1 to 10.
+                                // calling
+                              for (int i = 1; i <= 5; i++) {
+                                  w = w + 1;
+                              }
+        
+                                // each time through the loop
+        
+                            // this will tell the test how many times the loop executed.
+                            return w;
+                        }
+        
+                        public int oneToTen() {
+                            int w = 0;
+        
+                            // Write a FOR loop that counts from 1 to 10.
                             // calling
-                          for (int i = 1; i <= 5; i++) {
+                            for (int i = 1; i <= 10; i++) {
+                              w = w + 1;
+                            }
+        
+                            // each time through the loop
+        
+                            return w;
+                        }
+        
+                        public int startAtTwentyOne() {
+                            int w = 0;
+        
+                            // Write a FOR loop that makes 10 iterations, start at 21.
+                            // calling
+                            for (int i = 21; i <= 31; i++) {
+                              w = w + 1;
+                            }
+        
+                            // each time through the loop
+        
+                            return w;
+                        }
+        
+                        public int countDown() {
+                            int w = 0;
+        
+                            // Write a FOR loop that counts down from 100 to 0.
+                            // calling
+                            for (int i = 100; i > 0; i--) {
+                              w = w + 1;
+                            }
+        
+                            // each time through the loop
+        
+                            return w;
+                        }
+        
+                        public int byTwoTo32() {
+                            int w = 0;
+        
+                            // Write a FOR loop from 0 to 32 by 2s.
+                            // calling
+                            for (int i = 0; i < 32; i += 2) {
+                              w = w + 1;
+                            }
+        
+                            // each time through the loop
+                            return w;
+                        }
+        
+                        public int countDownFrom5000() {
+                            int w = 0;
+        
+                            // Write a FOR loop from 1 to less than 5001 by 11s.
+                            // calling
+                            for (int i = 1; i < 5001; i += 11) {
+                              w = w + 1;
+                            }
+        
+                            // each time through the loop
+        
+                            return w;
+                        }
+        
+                        public int nestedFors() {
+                            int w = 0;
+        
+                            // Write a nested FOR loop(s), where one counts from
+                            // 0 to less than 20 and the inner one counts from 0 to 4
+                                    // calling
+                                for (int i = 0; i < 20; i++) {
+                                  for (int j = 0; j <= 4; j++) {
+                                    w = w + 1;
+                                  }
+                                }
+        
+                                    // each time through the inner loop
+        
+                            return w;
+                        }
+        
+                        public int helloZipCode() {
+                            int w = 0;
+        
+                            // Write a FOR loop that counts from 5 to 105. Put an IF
+                            // statement inside the loop that checks the
+                            // loop index counter and if it’s greater than 51,
+                            // prints “Hello Zipcode” instead of the statement w = w + 1;
+        
+                                    // calling
+                                for (int i = 5; i <= 105; i++) {
+        
+                                  if (i > 51) {
+                                    System.out.println("Hello Zipcode");
+                                  } else {
+                                    w = w + 1;
+                                  }
+                                
+                                }
+        
+                                // each time through the inner loop
+        
+                            return w;
+                        }
+        
+                        public void simpleLoops() {
+                            int i = 0;
+        
+                            // sample while loop
+                            while (i <= 5) {
+                                System.out.println("Eww.");
+                                i = i + 1;
+                            }
+        
+                            // sample do...while loop
+                            i = 8;
+                            do {
+                                System.out.println("Eww.");
+                                i = i - 1;
+                            } while (i > 0);
+                            // what's the primary difference between them?!?
+                        }
+        
+                        // Write a WHILE loop that checks “gpsCurrentLocation()”
+                        // and if that is not equal to “Home” then and it calls “driveSomeMore()”.
+                        // After the loop is done, print “Honey, I’m Home!”
+                        public int driveHome() {
+                            int w = 0;
+                            while (!gpsCurrentLocation().equals("Home")) {
+                              driveSomeMore();
                               w = w + 1;
                           }
-    
-                            // each time through the loop
-    
-                        // this will tell the test how many times the loop executed.
-                        return w;
-                    }
-    
-                    public int oneToTen() {
-                        int w = 0;
-    
-                        // Write a FOR loop that counts from 1 to 10.
-                        // calling
-                        for (int i = 1; i <= 10; i++) {
-                          w = w + 1;
-                        }
-    
-                        // each time through the loop
-    
-                        return w;
-                    }
-    
-                    public int startAtTwentyOne() {
-                        int w = 0;
-    
-                        // Write a FOR loop that makes 10 iterations, start at 21.
-                        // calling
-                        for (int i = 21; i <= 31; i++) {
-                          w = w + 1;
-                        }
-    
-                        // each time through the loop
-    
-                        return w;
-                    }
-    
-                    public int countDown() {
-                        int w = 0;
-    
-                        // Write a FOR loop that counts down from 100 to 0.
-                        // calling
-                        for (int i = 100; i > 0; i--) {
-                          w = w + 1;
-                        }
-    
-                        // each time through the loop
-    
-                        return w;
-                    }
-    
-                    public int byTwoTo32() {
-                        int w = 0;
-    
-                        // Write a FOR loop from 0 to 32 by 2s.
-                        // calling
-                        for (int i = 0; i < 32; i += 2) {
-                          w = w + 1;
-                        }
-    
-                        // each time through the loop
-                        return w;
-                    }
-    
-                    public int countDownFrom5000() {
-                        int w = 0;
-    
-                        // Write a FOR loop from 1 to less than 5001 by 11s.
-                        // calling
-                        for (int i = 1; i < 5001; i += 11) {
-                          w = w + 1;
-                        }
-    
-                        // each time through the loop
-    
-                        return w;
-                    }
-    
-                    public int nestedFors() {
-                        int w = 0;
-    
-                        // Write a nested FOR loop(s), where one counts from
-                        // 0 to less than 20 and the inner one counts from 0 to 4
+                            System.out.println("Honey, I'm Home!");
+        
+                            // you need to use a .equals for two Strings.
+        
                                 // calling
-                            for (int i = 0; i < 20; i++) {
-                              for (int j = 0; j <= 4; j++) {
-                                w = w + 1;
-                              }
-                            }
-    
+                                //w = w + 1
                                 // each time through the inner loop
-    
-                        return w;
-                    }
-    
-                    public int helloZipCode() {
-                        int w = 0;
-    
-                        // Write a FOR loop that counts from 5 to 105. Put an IF
-                        // statement inside the loop that checks the
-                        // loop index counter and if it’s greater than 51,
-                        // prints “Hello Zipcode” instead of the statement w = w + 1;
-    
-                                // calling
-                            for (int i = 5; i < 105; i++) {
-    
-                              if (w > 51) {
-                                System.out.println("Hello Zipcode");
-                              } else {
-                                w = w + 1;
-                              }
-                            }
-    
-                            // each time through the inner loop
-    
-                        return w;
-                    }
-    
-                    public void simpleLoops() {
-                        int i = 0;
-    
-                        // sample while loop
-                        while (i <= 5) {
-                            System.out.println("Eww.");
-                            i = i + 1;
+        
+        
+                                return w;
                         }
-    
-                        // sample do...while loop
-                        i = 8;
-                        do {
-                            System.out.println("Eww.");
-                            i = i - 1;
-                        } while (i > 0);
-                        // what's the primary difference between them?!?
-                    }
-    
-                    // Write a WHILE loop that checks “gpsCurrentLocation()”
-                    // and if that is not equal to “Home” then and it calls “driveSomeMore()”.
-                    // After the loop is done, print “Honey, I’m Home!”
-                    public int driveHome() {
-                        int w = 0;
-                        while (!gpsCurrentLocation().equals("Home")) {
-                          driveSomeMore();
-                          w = w + 1;
-                      }
-                        System.out.println("Honey, I'm Home!");
-    
-                        // you need to use a .equals for two Strings.
-    
-                            // calling
-                            //w = w + 1
-                            // each time through the inner loop
-    
-    
-                            return w;
-                    }
-    
-                    // Getting harder...
-                    // First declare and set “highestScore” to 236. Then set “currentScore” to
-                    // “gameNextScore()”. Then write a WHILE loop that checks "runningScore"
-                    // is less than “highestScore” and if it is, adds “currentScore” to
-                    // "runningScore"
-                    // and then sets “currentScore” to “gameNextScore()”
-                    public int checkGameScore() {
-                        int w = 0;
-                        int highestScore = 236;
-                        int currentScore = gameNextScore();
-                        int runningScore = 0;
-    
-                        // do your while loop here
-                            while (runningScore < highestScore) {
-                                runningScore += currentScore;
-    
+        
+                        // Getting harder...
+                        // First declare and set “highestScore” to 236. Then set “currentScore” to
+                        // “gameNextScore()”. Then write a WHILE loop that checks "runningScore"
+                        // is less than “highestScore” and if it is, adds “currentScore” to
+                        // "runningScore"
+                        // and then sets “currentScore” to “gameNextScore()”
+                        public int checkGameScore() {
+                            int w = 0;
+                            int highestScore = 236;
+                            int currentScore = gameNextScore();
+                            int runningScore = 0;
+        
+                            // do your while loop here
+                                while (runningScore < highestScore) {
+                                    runningScore += currentScore;
+                                    currentScore = gameNextScore();
                             // calling
                                 w = w + 1;
                             // each time through the inner loop
@@ -212,11 +213,12 @@
                         // do your while loop here
                             do {
                                 runningScore += currentScore;
+                                currentScore = gameNextScore();
                         // calling
                                 w = w + 1;
                         // each time through the inner loop
                     } while (runningScore < highestScore);
-                return w; // >= 3;
+                return w >= 3;
             }
     
             // Write a WHILE loop that checks “serverIsRunning()” and if true
@@ -367,10 +369,14 @@
     public int tallyVote1() {
         int w = 0;
         int numberOfVotes = voteTallies.length;
-
+        
+            for (int i = 0; i < voteTallies.length; i++) {
+                System.out.println(voteTallies[i]);
+                w = w + 1;
+            }
 
             // calling
-            w = w + 1;
+            
             // each time through the inner loop
 
         return w;
@@ -382,10 +388,15 @@
     public int tallyVote2() {
         int w = 0;
         int numberOfVotes = voteTallies.length;
-
-
+        int i = 0;
+        
+            while (i < voteTallies.length) {
+                System.out.println(voteTallies[i]);
+                i++;
+                w = w + 1;
+            }
             // calling
-            w = w + 1;
+            //w = w + 1;
             // each time through the inner loop
 
         return w;
